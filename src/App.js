b6420dd3b73dec7ab3b6723475c3adbc4caa822c
Moura <hiroganja@gmail.com>
2022-05-02 16:25:07 -0300

    commit 002 :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
+      <h1 className="titulo">Buscador de CEP</h1>
 
       <div className="containerInput">
         <input
@@ -11,16 +12,19 @@ function App() {
         placeholder="Insira um CEP"
         />
 
-        <button className="buttonSearch">
-           <FiSearch size={25} color="white"/>
+        <button className="botaopesquisar">
+           <FiSearch size={25}/>
         </button>
       </div>
 
-      <main className='main'>
+      <main className='principal'>
         <h2>CEP: 01825-173</h2>
 
         <span>Rua para teste</span>
         <span>Completo: complemento para teste</span>
+        <span>Vila Romana</span>
+        <span>Sao Paulo</span>
+
       </main>
 
     </div>
